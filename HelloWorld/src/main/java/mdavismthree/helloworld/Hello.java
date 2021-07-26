0a8ef86f8ad03509607dc3a1224c8caeb5788945
Matt Davis <matt576.md@gmail.com>
2021-07-26 16:40:14 -0400

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/mdavismthree/helloworld/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mdavismthree.helloworld;
+
+/**
+ *
+ * @author Mathieu Davis
+ * email: matt576.md@gmail.com
+ * date: 7/26/21
+ * purpose: my first hello world program in Netbean
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Matt!");
+    }
+}
